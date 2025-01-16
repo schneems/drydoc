e1bd766f38df7269d626f401cddca4b379fe7165
Schneems <richard.schneeman+foo@gmail.com>
2025-01-16 16:58:21 -0600

    Parse into args with context

--- drydoc_derive/src/lib.rs
+++ drydoc_derive/src/lib.rs
@@ -1,40 +1,95 @@
-extern crate proc_macro;
-use std::path::PathBuf;
-
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, LitStr};
+use std::path::PathBuf;
+use syn::{
+    braced,
+    parse::{Parse, ParseStream},
+    parse_macro_input, Ident, LitStr, Token,
+};
 
-fn contents_from_input_path(input: LitStr) -> syn::Result<String> {
-    let cargo_dir =
-        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("cargo sets this env var"));
+/// "path/to/doc.rs", context: { number => 15 }
+#[derive(Debug, PartialEq, Eq)]
+struct DrydocArgs {
+    path: PathBuf,
+    context: Option<String>,
+}
 
-    let path = input.value();
-    fs_err::read_to_string(cargo_dir.join(&path)).map_err(|error| {
-        syn::Error::new(
-            input.span(),
-            format!("drydoc: Error reading from `{path}`:\n\n{error}"),
-        )
-    })
+impl Parse for DrydocArgs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        // Parse the file path
+        let path: LitStr = input.parse()?;
+        let path = PathBuf::from(path.value());
+
+        let _ = input.parse::<Token![,]>().ok();
+        let context = if input.peek(Ident) && input.peek2(Token![:]) {
+            let ident: Ident = input.parse()?;
+
+            if ident == "context" {
+                input.parse::<Token![:]>()?;
+                let content;
+                braced!(content in input);
+
+                // Consume the parse buffer and convert it into a string
+                let tokens: proc_macro2::TokenStream = content.parse()?;
+                Some(tokens.to_string())
+            } else {
+                return Err(syn::Error::new(ident.span(), "expected `context`"));
+            }
+        } else {
+            None
+        };
+
+        let args = DrydocArgs { path, context };
+        Ok(args)
+    }
+}
+
+struct Drydoc {
+    #[allow(dead_code)]
+    args: DrydocArgs,
+    contents: String,
+}
+
+impl Parse for Drydoc {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let args = DrydocArgs::parse(input)?;
+        let cargo_dir =
+            PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("cargo sets this env var"));
+
+        let contents = fs_err::read_to_string(cargo_dir.join(&args.path)).map_err(|error| {
+            syn::Error::new(
+                input.span(),
+                format!(
+                    "drydoc: Error reading from `{}`:\n\n{error}",
+                    args.path.display()
+                ),
+            )
+        })?;
+        Ok(Drydoc { args, contents })
+    }
 }
 
 /// The same thing as `include_str!` but it's relative from the crate root
 #[proc_macro]
 pub fn doc_show(input: TokenStream) -> TokenStream {
-    contents_from_input_path(parse_macro_input!(input as LitStr))
-        .map(|contents| quote! { #contents })
-        .unwrap_or_else(syn::Error::into_compile_error)
-        .into()
+    let Drydoc { args: _, contents } = parse_macro_input!(input as Drydoc);
+
+    quote! {
+        #contents
+    }
+    .into()
 }
 
 /// Reads a file relative from crate root and appends a doc hide token `#` to each line
 #[proc_macro]
 pub fn doc_hide(input: TokenStream) -> TokenStream {
-    contents_from_input_path(parse_macro_input!(input as LitStr))
-        .map(|contents| dochide_lines(&contents))
-        .map(|hidden| quote! {#hidden})
-        .unwrap_or_else(syn::Error::into_compile_error)
-        .into()
+    let Drydoc { args: _, contents } = parse_macro_input!(input as Drydoc);
+
+    let hidden = dochide_lines(&contents);
+    quote! {
+        #hidden
+    }
+    .into()
 }
 
 /// Prepends a doctest hide character `#` in front of each non-hidden line
@@ -56,6 +111,30 @@ fn dochide_lines(input: &str) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_parse_args() {
+        let input = r#""path/to/doc.rs", context: { number => 15 }"#;
+        let args: DrydocArgs = syn::parse_str(input).unwrap();
+
+        assert_eq!(args.path, PathBuf::from("path/to/doc.rs"));
+        assert_eq!(args.context, Some(r#"number => 15"#.to_string()));
+    }
+
+    #[test]
+    fn test_parse_args_no_context() {
+        let input = r#""path/to/doc.rs""#;
+        let args: DrydocArgs = syn::parse_str(input).expect("Failed to parse Args");
+        assert_eq!(args.path, PathBuf::from("path/to/doc.rs"));
+        assert_eq!(args.context, None);
+    }
+
+    #[test]
+    fn test_parse_bad_key_err() {
+        let input = r#""path/to/doc.rs", yolo: {}""#;
+        let result: syn::Result<DrydocArgs> = syn::parse_str(input);
+        assert!(result.is_err(), "Not err {result:?}");
+    }
 
     #[test]
     fn test_un_hidden_lines() {
